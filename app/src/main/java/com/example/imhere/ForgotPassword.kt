ec23fb45c7524b81d72f905401ec650a57628689
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2021-10-12 15:19:16 -0400

    feat: screens redirect

--- app/src/main/java/com/example/imhere/ForgotPassword.kt
+++ app/src/main/java/com/example/imhere/ForgotPassword.kt
@@ -1,11 +1,18 @@
 package com.example.imhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ForgotPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+    }
+
+    fun navigationScreenLogin(view : View) {
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/imhere/Login.kt
+++ app/src/main/java/com/example/imhere/Login.kt
@@ -1,11 +1,24 @@
 package com.example.imhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun navigationScreenRegister(view : View) {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+    }
+
+    fun navigationScreenForgotPassword(view : View) {
+        val intent = Intent(this, ForgotPassword::class.java)
+        startActivity(intent)
     }
 }
